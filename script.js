882abc3b1659ca3616236c7ff2438f90a7b7e1f8
Adrian Kossmann <adrian.kossmann@posteo.de>
2024-03-31 22:33:01 +0200

    ...

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const menu = document.getElementById('menu');
-//     const firstSection = document.getElementById('start-section');
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.getElementById('menu');
+    const firstSection = document.getElementById('start-section');
     
-//     // Setting rootMargin to be negative to start changing when the first section bottom is close to the menu
-//     const observerOptions = {
-//         root: null, // observing the viewport
-//         threshold: 0,
-//         rootMargin: "0px 0px -100% 0px" // Adjust based on menu height and desired effect
-//     };
+    // Setting rootMargin to be negative to start changing when the first section bottom is close to the menu
+    const observerOptions = {
+        root: null, // observing the viewport
+        threshold: 0,
+        rootMargin: "0px 0px -100% 0px" // Adjust based on menu height and desired effect
+    };
 
-//     const observer = new IntersectionObserver((entries) => {
-//         entries.forEach(entry => {
-//             // When the bottom of the first section is not intersecting the viewport (i.e., is above the viewport), make the menu sticky.
-//             if (!entry.isIntersecting) {
-//                 menu.classList.add('sticky');
-//             } else {
-//                 menu.classList.remove('sticky');
-//             }
-//         });
-//     }, observerOptions);
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            // When the bottom of the first section is not intersecting the viewport (i.e., is above the viewport), make the menu sticky.
+            if (!entry.isIntersecting) {
+                menu.classList.add('sticky');
+            } else {
+                menu.classList.remove('sticky');
+            }
+        });
+    }, observerOptions);
 
-//     // Start observing the bottom of the first section.
-//     observer.observe(firstSection);
-// });
+    // Start observing the bottom of the first section.
+    observer.observe(firstSection);
+});
 
 
 document.querySelectorAll('#menu a[href^="#"]').forEach(anchor => {
