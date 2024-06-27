513f112ca72006f43065a5a70018cd195c1d69f2
Adrian Kossmann <adrian.kossmann@posteo.de>
2024-06-27 23:04:31 +0200

    +content

--- script.js
+++ script.js
@@ -35,30 +35,30 @@ document.querySelectorAll('#menu a[href^="#"]').forEach(anchor => {
     });
 });
 
-var map = L.map('festival-map').setView([52.57000, 9.60968], 80); // Replace Latitude and Longitude with your festival's coordinates
+// var map = L.map('festival-map').setView([52.57000, 9.60968], 80); // Replace Latitude and Longitude with your festival's coordinates
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(map);
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '© OpenStreetMap contributors'
+// }).addTo(map);
 
-var marker = L.marker([52.57000, 9.60968],).addTo(map); // Replace Latitude and Longitude as above
-    marker.bindPopup("<b>Freaks of Rock</b>").openPopup();
+// var marker = L.marker([52.57000, 9.60968],).addTo(map); // Replace Latitude and Longitude as above
+//     marker.bindPopup("<b>Freaks of Rock</b>").openPopup();
 
-window.addEventListener('scroll', function() {
-    var scrollArrow = document.getElementById('scrollArrow');
-    // Adjust the divisor here for a slower fade-out effect
-    // For example, using 500 makes the fade-out more gradual
-    var opacity = 1 - (window.scrollY / 500);
-    opacity = Math.max(opacity, 0); // Ensure opacity doesn't go negative
+// window.addEventListener('scroll', function() {
+//     var scrollArrow = document.getElementById('scrollArrow');
+//     // Adjust the divisor here for a slower fade-out effect
+//     // For example, using 500 makes the fade-out more gradual
+//     var opacity = 1 - (window.scrollY / 500);
+//     opacity = Math.max(opacity, 0); // Ensure opacity doesn't go negative
 
-    scrollArrow.style.opacity = opacity.toString();
+//     scrollArrow.style.opacity = opacity.toString();
 
-    // Hide the arrow if almost fully transparent to prevent interaction
-    if (opacity <= 0.05) {
-        scrollArrow.style.display = 'none';
-    } else {
-        scrollArrow.style.display = 'block'; // Ensure it's shown again when scrolling back up
-    }
-});
+//     // Hide the arrow if almost fully transparent to prevent interaction
+//     if (opacity <= 0.05) {
+//         scrollArrow.style.display = 'none';
+//     } else {
+//         scrollArrow.style.display = 'block'; // Ensure it's shown again when scrolling back up
+//     }
+// });
 
 
